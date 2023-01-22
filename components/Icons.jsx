face34bfca1d3249a63d66e8b1ef5dbcfef3d3f0
Saurav Kumar <maxsaurav237@gmail.com>
2023-01-22 22:18:52 +0530

    added icons

---
+++ components/Icons.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+export function Icons({ name, className }) {
+  switch (name) {
+    case "backspace":
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={className}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z"
+          />
+        </svg>
+      );
+    case "dark":
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={className}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+          />
+        </svg>
+      );
+    case "light":
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={className}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+          />
+        </svg>
+      );
+    case "info":
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={className}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+          />
+        </svg>
+      );
+
+    default:
+      break;
+  }
+}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,15 @@
-import Link from "next/link";
 import React from "react";
+import { Icons } from "./Icons";
 import ThemeBtn from "./ThemeBtn";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 p-3 left-0 w-full text-black dark:text-white bg-sky-300 dark:bg-slate-900">
+    <nav className="sticky top-0 p-3 left-0 w-full text-black dark:text-white border-b bg-sky-200 dark:bg-slate-900">
       <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-        <Link href="/" className="text-xl font-bold">
-          Wordle
-        </Link>
+        <div>
+          <Icons name={"info"} className="w-6 h-6 stroke-2" />
+        </div>
+        <h1 className="text-2xl font-bold">Wordle</h1>
         <div>
           <ThemeBtn />
         </div>
